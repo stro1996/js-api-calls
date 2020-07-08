fcb31309a88450066f4955d351ff3a66d18551de
Sergey Tarasevich <stro1996@gmail.com>
2020-07-08 14:51:47 +0300

    add 3 methods

--- src/index.js
+++ src/index.js
@@ -1,20 +1,75 @@
 const BASE_URL = 'https://api.football-data.org/v2/';
+const GENERAL_ERROR = 'somethin went wrong';
 const header = {
   'X-Auth-Token': '94113bc113d64460bbc2f444a2b2a6cb',
 };
 
 async function getTeams() {
   try {
-    const data = await fetch(BASE_URL + 'teams', {
+    const data = await fetch(`${BASE_URL}teams`, {
       headers: {...header},
       method: 'GET',
     });
 
-    return await data.json();
+    if (data.ok) {
+      const allData = await data.json();
+      return allData.teams;
+    } else {
+      throw GENERAL_ERROR
+    }
   } catch (error) {
-    return error;
+    if (typeof error === string) {
+      throw error;
+    } else {
+      throw GENERAL_ERROR
+    }
+  }
+}
+
+async function getTeam(id) {
+  try {
+    const data = await fetch(`${BASE_URL}teams/${id}`, {
+      headers: {...header},
+      method: 'GET',
+    });
+
+    if (data.ok) {
+      return await data.json();
+    } else {
+      throw GENERAL_ERROR
+    }
+  } catch (error) {
+    if (typeof error === string) {
+      throw error;
+    } else {
+      throw GENERAL_ERROR
+    }
+  }
+}
+
+async function getUpcomingMatches(id) {
+  try {
+    const data = await fetch(`${BASE_URL}teams/${id}/matches?status=SCHEDULED`, {
+      headers: {...header},
+      method: 'GET',
+    });
+
+    if (data.ok) {
+      const allData = await data.json();
+
+      return allData.matches;
+    } else {
+      throw GENERAL_ERROR
+    }
+  } catch (error) {
+    if (typeof error === string) {
+      throw error;
+    } else {
+      throw GENERAL_ERROR
+    }
   }
 }
 
 
-export { getTeams };
+
+export {getTeams, getTeam, getUpcomingMatches};
