4f235a2719c202f7451ef1e3cdc928e8652fbd95
Sergey Tarasevich <stro1996@gmail.com>
2020-07-08 17:08:35 +0300

    add axios

--- src/index.js
+++ src/index.js
@@ -1,75 +1,46 @@
+import axios from 'axios';
 const BASE_URL = 'https://api.football-data.org/v2/';
 const GENERAL_ERROR = 'somethin went wrong';
 const header = {
+  'content-type': 'application/json',
   'X-Auth-Token': '94113bc113d64460bbc2f444a2b2a6cb',
 };
 
-async function getTeams() {
-  try {
-    const data = await fetch(`${BASE_URL}teams`, {
-      headers: {...header},
-      method: 'GET',
-    });
+const getTeams = async () => {
+  const data = await axios.get(`${BASE_URL}/teams`, {
+    headers: {...header}
+  });
 
-    if (data.ok) {
-      const allData = await data.json();
-      return allData.teams;
-    } else {
-      throw GENERAL_ERROR
-    }
-  } catch (error) {
-    if (typeof error === string) {
-      throw error;
-    } else {
-      throw GENERAL_ERROR
-    }
+  if (data.ok && data && data.data && data.data.teams) {
+    return data.data.teams;
+  } else {
+    throw GENERAL_ERROR;
   }
-}
+};
 
-async function getTeam(id) {
-  try {
-    const data = await fetch(`${BASE_URL}teams/${id}`, {
-      headers: {...header},
-      method: 'GET',
-    });
+const getTeam = async (id) => {
+  const data = await axios.get(`${BASE_URL}${id}`, {
+    headers: {...header}
+  });
 
-    if (data.ok) {
-      return await data.json();
-    } else {
-      throw GENERAL_ERROR
-    }
-  } catch (error) {
-    if (typeof error === string) {
-      throw error;
-    } else {
-      throw GENERAL_ERROR
-    }
+  if (data.ok && data && data.data) {
+    return data.data;
+  } else {
+    throw GENERAL_ERROR;
   }
-}
+};
 
-async function getUpcomingMatches(id) {
-  try {
-    const data = await fetch(`${BASE_URL}teams/${id}/matches?status=SCHEDULED`, {
-      headers: {...header},
-      method: 'GET',
-    });
+const getUpcomingMatches = async (id) => {
+  const data = await axios.get(`${BASE_URL}teams/${id}/matches?status=SCHEDULED`, {
+    headers: {...header}
+  });
 
-    if (data.ok) {
-      const allData = await data.json();
 
-      return allData.matches;
-    } else {
-      throw GENERAL_ERROR
-    }
-  } catch (error) {
-    if (typeof error === string) {
-      throw error;
-    } else {
-      throw GENERAL_ERROR
-    }
+  if (data.ok && data && data.data && data.data.matches) {
+    return data.data.matches;
+  } else {
+    throw GENERAL_ERROR;
   }
-}
-
-
+};
 
 export {getTeams, getTeam, getUpcomingMatches};
